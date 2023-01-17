578855aa5b1bb42676e43841fcb4ff40c0a9d8dd
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-17 20:10:54 +0200

    Add validation of password more than eight chars

--- password_validator.py
+++ password_validator.py
@@ -1,3 +1,8 @@
 class PasswordValidator():
     def validate(self, password: str):
-        return ""
+        error_string = ""
+        
+        if len(password) < 8:
+            error_string = "Password must be at least 8 charachters"
+        
+        return error_string
--- password_validator_test.py
+++ password_validator_test.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 from password_validator import PasswordValidator
 
 class TestPasswordValidator(TestCase):
-    def test_create_validator(self):
+    def test_validate_password_is_atleast_eight_chars_long(self):
         password_validator = PasswordValidator()
-        self.assertEqual(password_validator.validate(""), "")
+        error_string = password_validator.validate("qwer")
+        self.assertEqual(error_string, "Password must be at least 8 charachters")
