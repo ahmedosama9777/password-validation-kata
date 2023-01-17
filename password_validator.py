cbe8a742f53d926c3e8020f25dbf25e216df2ee3
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-17 20:21:07 +0200

    Add validation of password contatining at least two numbers

--- password_validator.py
+++ password_validator.py
@@ -1,8 +1,28 @@
-class PasswordValidator():
-    def validate(self, password: str):
+class PasswordValidator:
+    def __init__(self, password: str):
+        self.password = password
+
+    def validate(self):
         error_string = ""
-        
-        if len(password) < 8:
+
+        if not self._more_than_eight_charachters():
             error_string = "Password must be at least 8 charachters"
-        
+        elif not self._has_two_numbers():
+            error_string = "The password must contain at least 2 numbers"
         return error_string
+
+    def _more_than_eight_charachters(self):
+        if len(self.password) < 8:
+            return False
+
+        return True
+
+    def _has_two_numbers(self):
+        numbers = 0
+        for char in self.password:
+            if char.isnumeric():
+                numbers += 1
+        if numbers < 2:
+            return False
+
+        return True
--- password_validator_test.py
+++ password_validator_test.py
@@ -2,8 +2,14 @@ from unittest import TestCase
 
 from password_validator import PasswordValidator
 
+
 class TestPasswordValidator(TestCase):
     def test_validate_password_is_atleast_eight_chars_long(self):
-        password_validator = PasswordValidator()
-        error_string = password_validator.validate("qwer")
+        password_validator = PasswordValidator(password="qwer")
+        error_string = password_validator.validate()
         self.assertEqual(error_string, "Password must be at least 8 charachters")
+
+    def test_validate_password_contains_two_or_more_numbers(self):
+        password_validator = PasswordValidator(password="1qwertyu")
+        error_string = password_validator.validate()
+        self.assertEqual(error_string, "The password must contain at least 2 numbers")
