439c0e7ea740ea3af3b2f23e922e9e0014fbbf8e
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-17 20:40:01 +0200

    Add validation that password has at least 1 capital letter

--- password_validator.py
+++ password_validator.py
@@ -10,6 +10,9 @@ class PasswordValidator:
 
         if not self._has_two_numbers():
             error_string += "\n" + "The password must contain at least 2 numbers"
+        
+        if not self._has_one_capital_letter():
+            error_string += "\n" + "The password must contain at least 1 capital letter"
 
         return False if error_string else True, error_string
 
@@ -28,3 +31,10 @@ class PasswordValidator:
             return False
 
         return True
+
+    def _has_one_capital_letter(self):
+        for char in self.password:
+            if char.isupper():
+                return True
+        
+        return False
--- password_validator_test.py
+++ password_validator_test.py
@@ -5,22 +5,28 @@ from password_validator import PasswordValidator
 
 class TestPasswordValidator(TestCase):
     def test_validate_password_is_atleast_eight_chars_long(self):
-        password_validator = PasswordValidator(password="qwer12")
+        password_validator = PasswordValidator(password="Qwer12")
         valid, error_string = password_validator.validate()
         self.assertEqual(valid, False)
         self.assertEqual(error_string, "\nPassword must be at least 8 charachters")
 
     def test_validate_password_contains_two_or_more_numbers(self):
-        password_validator = PasswordValidator(password="1qwertyu")
+        password_validator = PasswordValidator(password="1qwerTyu")
         valid, error_string = password_validator.validate()
         self.assertEqual(valid, False)
         self.assertEqual(error_string, "\nThe password must contain at least 2 numbers")
 
     def test_handle_multiple_validation_errors(self):
-        password_validator = PasswordValidator(password="qwer")
+        password_validator = PasswordValidator(password="qQer")
         valid, error_string = password_validator.validate()
         self.assertEqual(valid, False)
         self.assertEqual(
             error_string,
             "\nPassword must be at least 8 charachters\nThe password must contain at least 2 numbers",
         )
+
+    def test_validate_password_contains_one_or_more_capital_letter(self):
+        password_validator = PasswordValidator(password="qwerty12")
+        valid, error_string = password_validator.validate()
+        self.assertEqual(valid, False)
+        self.assertEqual(error_string, "\nThe password must contain at least 1 capital letter")
